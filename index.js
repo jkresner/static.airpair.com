914cb14ac0779e0d2d668a32d55c5aff8d923c81
Jonathon Kresner <jkresner@gmail.com>
2015-10-10 09:55:25 +1100

    Add in a little color

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var colors = require('colors')
 var path = require('path')
 var http = require('http')
 var finalhandler = require('finalhandler')
@@ -9,5 +10,7 @@ var serve = serveStatic(path.join(__dirname,'public'))
 var server = http.createServer(function(req, res) {
   serve(req, res, finalhandler(req, res))
 })
+
+console.log('Listening on:'.gray + port.toString().white)
 
 server.listen(port)
