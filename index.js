801eca2800ac9a11a53b444227d7ee40049cf05d
Jonathon Kresner <jkresner@gmail.com>
2015-10-21 05:04:52 -0700

    Add icons and logging

--- index.js
+++ index.js
@@ -4,13 +4,26 @@ var http = require('http')
 var finalhandler = require('finalhandler')
 var serveStatic = require('serve-static')
 
+var env = process.env.ENV || 'dev'
 var port = process.env.PORT || 8000
-var serve = serveStatic(path.join(__dirname,'public'))
+var serveOps = {fallthrough:false}
+var staticDir = path.join(__dirname,'public')
+var serve = serveStatic(staticDir)
 
-var server = http.createServer(function(req, res) {
-  serve(req, res, finalhandler(req, res))
+var server = http.createServer(function(req, res) {  
+  
+  console.log(req.url)
+
+  var opts = {
+    env: env,
+    onerror: function(err, req, res) { console.log(req.url.red) }
+  }
+  
+  serve(req, res, finalhandler(req, res, opts))
+
 })
 
-console.log('Listening on:'.gray + port.toString().white)
-
-server.listen(port)
+console.log('Try listen on: '.gray, port.toString().white, staticDir)
+server.listen(port, () => {
+  console.log('Listening on: '.gray + port.toString().white)
+})
